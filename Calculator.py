45b771d5bcedce687bf3b7fb4b459bf7255056af
Habibur Rahman <hbrajon890@gmail.com>
2025-07-26 16:52:27 +0600

    first commit

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Multiply")
+print("4 - Divide")
+option = int(input("Choose an operation: "))
+
+if(option in [1,2,3,4]):
+    number1 = int(input("Enter the first number:"))
+    number2 = int(input("Enter the second number: "))
+
+    if(option == 1):
+        result = number1 + number2
+    elif(option == 2):
+        result = number1 - number2 
+    elif(option == 3):
+        result= number1 * number2
+    elif(option == 4):
+        result = number1 // number2    #// mane sudu int 
+
+
+
+else:
+    print("Invalid number")
+
+print(f"the result of this operation is : {result}")
